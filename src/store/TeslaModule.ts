2459b0aa0f33ed0eeec8ce9ffd7f5fd0b1b63a5f
ligeti.tamas <ltommipresents@gmail.com>
2021-01-13 17:09:46 +0100

    :fire: Removed template components

--- src/store/TeslaModule.ts
+++ src/store/TeslaModule.ts
@@ -3,16 +3,7 @@ import { Action, Module, Mutation, VuexModule } from "vuex-module-decorators";
 
 @Module
 export default class TeslaModule extends VuexModule {
-  // State (temp fields, direct access from store):
-  private __showEditForm: boolean = false;
-  private __editedFood: any;
-  private __currentPage: number = 1; // aktuálisan megjelenő oldal sorszáma
-  private __perPage: number = 5; // oldalanként megjelenő rekordok száma
-  private __polling: number;
-
   // State (classic fields)
-  private _foods: any = [];
-  private _topFoods: any = [];
   private _cars: any = [];
   private _model: any = [];
   private _carId: any = "";
@@ -36,18 +27,6 @@ export default class TeslaModule extends VuexModule {
   }
   get carId(): any {
     return this._carId;
-  }
-
-  get foods(): any[] {
-    return this._foods;
-  }
-
-  get topFoods(): any[] {
-    return this._topFoods;
-  }
-
-  get numberOfFoods(): number {
-    return this.foods.length;
   }
 
   @Action
@@ -95,81 +74,7 @@ export default class TeslaModule extends VuexModule {
       alert("A konfigurációja elérhető a következő id-val: " + id);
     });
   }
-  @Action
-  public async getFoods(): Promise<any> {
-    axios
-      .get("/csudijo", this.config)
-      .then((res: AxiosResponse) => {
-        const data: any = res.data;
-        if (data) {
-          // Mutáció hívása (commit())
-          this.context.commit("mutateFoods", data);
-          // További akciók hívására van lehetőség (dispatch()):
-          this.context.dispatch("getTopFoodsList");
-        }
-      })
-      .catch((ex: AxiosError) => alert(ex.message));
-  }
 
-  @Action
-  public async getTopFoodsList(): Promise<any> {
-    axios
-      .get("/csudijobest", this.config)
-      .then((res: AxiosResponse) => {
-        const data: any = res.data;
-        this.context.commit("mutateTopFoods", data);
-      })
-      .catch((ex: AxiosError) => alert(ex.message));
-  }
-
-  @Action
-  public async addNewFood(newFood: any): Promise<any> {
-    axios
-      .post("/csudijo", newFood, this.config)
-      .then((res: AxiosResponse) => {
-        if (res.data.errmsg || res.data.message) {
-          alert(res.data.errmsg ? res.data.errmsg : res.data.message);
-        } else {
-          this.context.dispatch("getFoods");
-        }
-      })
-      .catch((ex: AxiosError) => alert(ex.message));
-  }
-
-  @Action
-  public async updateFood(food: any): Promise<any> {
-    axios
-      .put(`/csudijo/${food._id}`, food, this.config) // update kezdeményezése
-      .then((res: AxiosResponse) => {
-        if (res.data.errmsg || res.data.message) {
-          alert(res.data.errmsg ? res.data.errmsg : res.data.message);
-        } else {
-          this.context.dispatch("getFoods");
-        }
-      })
-      .catch((ex: AxiosError) => alert(ex.message));
-  }
-
-  @Action
-  public async deleteFood(food: any): Promise<any> {
-    if (confirm("Are you sure you want to delete this food?")) {
-      axios
-        .delete(`/csudijo/${food._id}`, this.config)
-        .then((res: AxiosResponse) => {
-          if (res.data.errmsg) {
-            alert(res.data.errmsg);
-          }
-          this.context.dispatch("getFoods");
-          alert(res.data.message);
-        })
-        .catch((ex: AxiosError) => alert(ex.message));
-    }
-  }
-
-  @Mutation
-  private mutateFoods(data: any): void {
-    this._foods = data;
-  }
   @Mutation
   private mutateModel(data: any): void {
     this._model = data;
@@ -182,16 +87,5 @@ export default class TeslaModule extends VuexModule {
   @Mutation
   private mutateCars(data: any): void {
     this._cars = data;
-  }
-
-  @Mutation
-  private mutateTopFoods(data: any): void {
-    this._topFoods = [];
-    if (!data.error) {
-      // 9. feladat:
-      // Itt még "átjön" a "top" ételek minden adata, ami felesleges, mert
-      // csak a nevükre van szükségünk
-      this._topFoods = data.map((a: any) => a.foodName);
-    }
   }
 }
